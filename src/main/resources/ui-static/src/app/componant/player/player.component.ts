893b9f18513f0a43910c8e3ac01bbe7854006504
ashraf <ashraf1abdelrasool@gmail.com>
2018-12-03 19:58:50 +0200

    push all :fire:

--- src/main/resources/ui-static/src/app/componant/player/player.component.ts
+++ src/main/resources/ui-static/src/app/componant/player/player.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 declare var jwplayer: any;
+declare var Clappr: any;
+declare var LevelSelector: any;
 
 @Component({
   selector: 'as-player',
@@ -15,11 +17,35 @@ export class PlayerComponent implements OnInit {
   }
 
   ngOnInit() {
+/*
     jwplayer('player').setup(
       {
         "image": 'https://df30el3rzhrdb.cloudfront.net/' + this.id + '.png',
         "file": '/tube/api/' + this.id + '.m3u8/',
       }
     );
+*/
+
+
+
+
+    var player = new Clappr.Player({source: '/tube/api/' + this.id + '.m3u8/', parentId: "#player",
+      plugins: [Clappr.FlasHLS,LevelSelector],
+      height: 360,
+      width: 640
+      ,
+
+      levelSelectorConfig: {
+        title: 'Quality',
+        labelCallback: function(playbackLevel, customLabel) {
+          return  playbackLevel.level.height+'p'; // High 720p
+        }
+      }
+      ,poster: {
+        url: 'https://df30el3rzhrdb.cloudfront.net/' + this.id + '.png',
+        showOnVideoEnd: false
+      }
+    });
+
   }
 }
